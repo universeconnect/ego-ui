5d6ba3089e6cf9189191d203d07ad7c334c74c93
Jcuser <2550960513@qq.com>
2019-09-04 23:37:32 +0800

    关于我们introduction简单

--- src/components/index/home/home/the-home-intro.vue
+++ src/components/index/home/home/the-home-intro.vue
@@ -1,6 +1,34 @@
 <template>
     <div class="box01">
-
+      <el-row >
+        <el-col class="intro-left-box">
+          <el-card class="intro-card" shadow="never">
+            <h2>关于我们</h2>
+            <p style="letter-spacing: 2px; line-height: 26px">
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;工作室
+              成立与2018年，	富有活力，集创意、设计、
+              策划、制作于一体的设计性机构。主要从事产品设
+              计开发及生产、空间规划、品牌策划、包装及礼品
+              设计。他们热情真诚，为需要的客户量身设计和开
+              发定制产品。他们参与的设计注重设计意识和创意
+              的传播及设计流程的管理，更注重同行业间的沟通
+              和设计意识的更新。</p>
+          </el-card>
+        </el-col>
+        <el-col class="intro-right-box">
+          <el-carousel :interval="4000" type="card" height="310px">
+            <el-carousel-item class="intro-card-ca" v-for="item in 6" :key="item">
+              <el-card shadow="always" class="intro-card-nei">
+                <div class="intro-card-radio">
+                  <img src="../../../../assets/default_imgs/tzb.jpg" width="150px" height="150px" style="border-radius: 50%"/>
+                </div>
+                <h4 class="center">挑战杯优秀奖</h4>
+                <p>挑战杯是全国大学生课外科技作品竞赛。指导老师:陈妍</p>
+              </el-card>
+            </el-carousel-item>
+          </el-carousel>
+        </el-col>
+      </el-row>
     </div>
 </template>
 
@@ -11,5 +39,56 @@
 </script>
 
 <style scoped>
+.box01{
+  width: 100%;
+  height: 500px;
+  background-color: lightgray;
+}
+  .intro-left-box{
+    width: 35%;
+    height: 500px;
+    margin-left: 7%;
+  }
+  .intro-card{
+    height: 390px;
+    margin-top: 20px;
+    padding: 50px 40px 10px 50px;
+  }
+  .intro-right-box{
+    width: 45%;
+    height: 500px;
+    margin-left: 7%;
+    padding-top: 80px;
+  }
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 200px;
+  margin: 0;
+}
 
+  .intro-card-ca{
+    width: 400px;
+  }
+  .intro-card-nei{
+    height: 310px;
+    width: 90%;
+  }
+  .intro-card-radio{
+    width: 150px;
+    height: 150px;
+    margin: 0 auto;
+
+
+  }
+  .intro-card-radio img{
+    z-index: -1;
+  }
+  .center{
+    width: auto;
+    text-align: center;
+    padding-top: 5px;
+    margin: 0 auto;
+  }
 </style>
--- src/components/index/loginAndSign/login/the-login.vue
+++ src/components/index/loginAndSign/login/the-login.vue
@@ -1,17 +1,25 @@
 <template>
     <div>
-        <h1>这是登录组件</h1>
+        <!--<h1>这是登录组件</h1>
         <button @click="oklogin">点击登录成功</button>
         <button @click="nologin">点击注销登录</button>
         <h2 v-if="$store.state.isLogin">现在是登录状态</h2>
         <h2 v-if="!$store.state.isLogin">现在是未登录状态</h2>
-        <router-link :to="{path: '/index/home'}">回到首页</router-link>
+        <router-link :to="{path: '/index/home'}">回到首页</router-link>-->
+      <el-card shadow="hover" class="login-box">
+        鼠标悬浮时显示
+      </el-card>
     </div>
 </template>
 
 <script>
     export default {
         name: "login",
+      data(){
+        return{
+
+        }
+      },
         methods:{
             oklogin(){
                 //改变登录状态
@@ -26,5 +34,9 @@
 </script>
 
 <style scoped>
-
+.login-box{
+  width: 500px;
+  height: 400px;
+  margin-top: 40px;
+}
 </style>
