7bfa33a779b0af9456a070d93ad7a4ce585513da
Wing979 <2569217348@qq.com>
2019-09-08 16:39:51 +0800

    Merge branch 'ubsidiary' of C:\Users\歪歪斜斜\WebstormProjects\our-studio-ui1 with conflicts.

--- src/components/index/home/home/the-home-below.vue
+++ src/components/index/home/home/the-home-below.vue
@@ -1,6 +1,34 @@
 <template>
-    <div class="box01">
-
+    <div class="footer">
+      <div>
+        <div class="links">
+          <dl>
+            <dt>关于我们</dt>
+            <dd><a href="#">关于</a></dd>
+            <dd><a href="#">关于我们</a></dd>
+            <dd><a href="#">关于我们啊</a></dd>
+            <dd><a href="#">关于我们</a></dd>
+          </dl>
+          <dl>
+            <dt>关于我们</dt>
+            <dd><a href="#">关于</a></dd>
+            <dd><a href="#">关于我们</a></dd>
+            <dd><a href="#">关于我们啊</a></dd>
+            <dd><a href="#">关于我们</a></dd>
+          </dl>
+          <dl>
+            <dt>关于我们</dt>
+            <dd><a href="#">关于</a></dd>
+            <dd><a href="#">关于我们</a></dd>
+            <dd><a href="#">关于我们啊</a></dd>
+            <dd><a href="#">关于我们</a></dd>
+          </dl>
+        </div>
+      </div>
+      <img src="erweima.jpg" alt="">
+      <div class="right">
+        <img src="" alt="">
+      </div>
     </div>
 </template>
 
@@ -11,5 +39,38 @@
 </script>
 
 <style scoped>
+  a {
+    text-decoration: none;
+  }
+ .footer {
+   width: 100%;
+   height: 200px;
+   background-color: #201e1e;
+ }
+ .links {
+   margin-left: 54px;
+ }
+  .links dl {
+    float: left;
+    padding-top: 27px;
+    margin-right: 70px;
+  }
+  .links dt {
+    height: 35px;
+    font-size: 16px;
+    color: #e5e5e5;
+    text-align: right;
+  }
+  .links dd a {
+    text-align: left;
+    font-size: 12px;
+    color: #e5e5e5;
 
+  }
+  .links dd a:hover {
+    color: #d73d07;
+  }
+  .right {
+    float: right;
+  }
 </style>
